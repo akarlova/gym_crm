488e470bc14dba9a643adc693939afba4ed9c4fd
aka.rlova <ladyerial88@gmail.com>
2025-09-28 23:21:29 +0300

    tests added

--- src/main/java/com/epam/gym_crm/Main.java
+++ src/main/java/com/epam/gym_crm/Main.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(AppConfig.class)) {
             log.info("Spring context up. Beans: {}", context.getBeanDefinitionCount());
-
         }
     }
 }
