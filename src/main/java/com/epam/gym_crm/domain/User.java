9c22d07294a73c06581469c1f9302136e84b6ac7
aka.rlova <ladyerial88@gmail.com>
2025-10-13 01:23:20 +0300

    alteration domain layer into entities

--- src/main/java/com/epam/gym_crm/domain/User.java
+++ src/main/java/com/epam/gym_crm/domain/User.java
@@ -1,22 +1,60 @@
 package com.epam.gym_crm.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
-public abstract class User {
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    @NotBlank
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    @NotBlank
     private String lastName;
+
+    @Column(name = "username", nullable = false, unique = true, length = 120)
+    @Size(max = 120)
+    @NotBlank
     private String username;
+
+    @Column(name = "password", nullable = false)
+    @NotBlank
     private String password;
+
+    @Column(name = "is_active", nullable = false)
     private boolean active = true;
+
+    @OneToOne(mappedBy = "user")
+    private Trainee trainee;
+
+    @OneToOne (mappedBy = "user")
+    private Trainer trainer;
+
+    protected User() {
+    }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
 
     public String getFirstName() {
         return firstName;
@@ -58,11 +96,18 @@ public abstract class User {
         this.active = active;
     }
 
+    public Trainee getTrainee() {
+        return trainee;
+    }
+
+    public Trainer getTrainer() {
+        return trainer;
+    }
+
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
         if (object == null || getClass() != object.getClass()) return false;
-
         User user = (User) object;
         return id != null && id.equals(user.id);
     }
