ccf67b339c4d8df2b9414d305f2520b7b0c21efb
aka.rlova <ladyerial88@gmail.com>
2025-10-16 04:19:34 +0300

    Main class had been cleared

--- src/main/java/com/epam/gym_crm/Main.java
+++ src/main/java/com/epam/gym_crm/Main.java
@@ -1,9 +1,35 @@
 package com.epam.gym_crm;
 
 import com.epam.gym_crm.config.AppConfig;
+import com.epam.gym_crm.config.HibernateUtil;
+import com.epam.gym_crm.domain.Trainee;
+import com.epam.gym_crm.domain.Trainer;
+import com.epam.gym_crm.domain.Training;
+import com.epam.gym_crm.domain.TrainingType;
+import com.epam.gym_crm.domain.User;
+import com.epam.gym_crm.facade.GymFacade;
+import com.epam.gym_crm.repository.ITrainingRepository;
+import com.epam.gym_crm.repository.impl.TraineeRepositoryImpl;
+import com.epam.gym_crm.repository.impl.TrainerRepositoryImpl;
+import com.epam.gym_crm.repository.impl.TrainingRepositoryImpl;
+import com.epam.gym_crm.service.ITraineeService;
+import com.epam.gym_crm.service.ITrainerService;
+import com.epam.gym_crm.service.ITrainingService;
+import com.epam.gym_crm.service.impl.AuthServiceImpl;
+import com.epam.gym_crm.service.impl.TraineeServiceImpl;
+import com.epam.gym_crm.service.impl.TrainerServiceImpl;
+import com.epam.gym_crm.service.impl.TrainingServiceImpl;
+import com.epam.gym_crm.util.impl.BasePasswordGenerator;
+import com.epam.gym_crm.util.impl.SimpleUsernameGenerator;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 public class Main {
@@ -12,7 +38,9 @@ public class Main {
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(AppConfig.class)) {
             log.info("Spring context up. Beans: {}", context.getBeanDefinitionCount());
-            var log = org.slf4j.LoggerFactory.getLogger("Main");
+            var log = LoggerFactory.getLogger("Main");
+
         }
     }
 }
+
